e497de6c0cbfcd92519dd930c8b387b5c35b0000
Deepakjen <112792076+Deepakjen@users.noreply.github.com>
2025-07-26 23:24:39 +0530

    My first commit

---
+++ java_Basic/src/java_Basic_Program/First_program.java
@@ -0,0 +1,14 @@
+package java_Basic_Program;
+
+
+
+public class First_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
